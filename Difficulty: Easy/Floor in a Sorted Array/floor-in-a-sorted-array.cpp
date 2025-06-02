751f8fbd5da9ea225640a485a33948980b8fe758
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2025-06-02 16:52:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -7,19 +7,17 @@ class Solution {
         int right = arr.size() - 1;
         int mid = 0;
         int ans = -1;
+        
         while(left<=right){
             mid = left + (right - left)/2;
-
-            if (arr[mid]<=x){
+            
+            if (x>=arr[mid]){
                 ans = mid;
                 left = mid + 1;
-                
             }
-            else if (arr[mid]>x){
+            else
                 right = mid - 1;
-            }
         }
-        
         return ans;
     }
 };
