df2ab9e8cbc4f6be19effbbff1838637c087a533
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2025-06-02 17:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (41.82%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,20 +3,19 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
+        int index = nums.size();
         int mid = 0;
         while (left<=right){
-            mid = left + (right-left)/2;
-            if (nums[mid]==target){
-                return mid;
-            }
-            else if (nums[mid]<target){
-                left = mid + 1;
-            }
-            else if (nums[mid] >target){
+            mid = left + (right - left)/2;
+
+            if (nums[mid]>=target){
+                index = mid;
                 right = mid - 1;
             }
-
+            else
+                left = mid + 1;
         }
-        return left;
+
+        return index;
     }
 };
