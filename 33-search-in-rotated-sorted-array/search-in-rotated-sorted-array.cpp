fe25f429f9a523147b96996773b92a8687640ee1
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2025-06-03 22:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.96%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int maxi = INT_MIN;
-        int right = nums.size()-1;
+
+        int mid = 0;
         int left = 0;
-        int mid = -1;
-        int index = -1;
-        for(int i=0; i< nums.size(); i++){
-            if (maxi<nums[i]){
-                maxi = nums[i];
-                index = i;
+        int right = nums.size() - 1;
+
+        while(left<=right){
+            mid = left + (right - left)/2;
+            
+            if (nums[mid]==target)
+                return mid;
+
+            else if (nums[left]<=nums[mid]){
+                if (target<=nums[mid] && nums[left]<=target)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+                    
             }
-        }
-        
-        right = index;
-
-        while (left<=right){
-            mid = left + (right - left)/2;
-            if (nums[mid]==target)
-                return mid;
-            else if (nums[mid]<target)
-                left = mid + 1;
             else
-                right = mid - 1;
-        }
-
-        left = index + 1;
-        right = nums.size() - 1;
-
-        while (left<=right){
-            mid = left + (right - left)/2;
-            if (nums[mid]==target)
-                return mid;
-            else if (nums[mid]<target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+                if (target>=nums[mid] && nums[right]>=target)
+                    left = mid + 1;
+                else
+                    right = mid-1;
         }
 
         return -1;
