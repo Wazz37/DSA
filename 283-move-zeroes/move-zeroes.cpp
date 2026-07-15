ab834435b2104dcf44c316088519700c3561d47f
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2026-07-15 22:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k = 0;
+        int temp;
+        int j = 0;
         for(int i = 0; i < nums.size(); i++){
             if (nums[i] != 0){
-                swap(nums[i], nums[k]);
-                k++;
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
         }
     }
