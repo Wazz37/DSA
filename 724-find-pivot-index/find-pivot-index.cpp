7bc37ab90566f1363ecc6b9f92e9ea1b3c50811b
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2026-07-25 13:23:07 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (30.49%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int total_sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            total_sum = nums[i] + total_sum;
-        }
         int left = 0;
         int right = 0;
+        int total_sum = 0;
+
         for(int i = 0; i < nums.size(); i++){
-            // total_sum = left + right + nums[i];
-            right = total_sum - nums[i] - left;
-            // std::cout << "right : " << right << std::endl;
-           if (left == right){
-                return i;
-            }
-            left = left + nums[i];
-            // std::cout << "left : " << left << std::endl;
- 
+            total_sum = total_sum + nums[i];
         }
 
-        // std::cout << "total sum : " << total_sum << std::endl;
+
+        for(int i = 0; i < nums.size(); i++){
+            right = total_sum - left - nums[i];
+
+            if (left == right){
+                return i;
+            }
+
+            left = left + nums[i];
+
+
+        }
         return -1;
-
     }
-
 };
