1dd08ef4136f3ac8dff4d7146f9b7b51bb1be2a9
MD Wazid Ansari <80128529+Wazz37@users.noreply.github.com>
2026-07-30 13:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (58.10%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int left = 0;
+        int prod = 1;
+        int ans = 0;
+
+        if (nums.size() == 1){
+            if (nums[0] >= k) return 0;
+            else return 1;
+        }
+        
+        for(int right = 0; right < nums.size(); right++){
+            prod = prod * nums[right];
+
+            while (prod >= k){
+                prod = prod/nums[left]; 
+                left++;
+            }
+            ans = ans + (right - left + 1);
+        }
+
+        return ans;
+    }
+};
